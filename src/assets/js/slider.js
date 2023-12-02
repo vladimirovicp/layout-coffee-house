44d771a86f32df2f279deb87a3a8bbf0f8065471
vladimirovicp <vladimirovicp70@gmail.com>
2023-12-02 23:40:08 +0400

    feat: slider прокрутка

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -12,12 +12,30 @@ if (slider) {
     let positionSlide = 0;
 
     arrows.forEach(arrow => {
-
         arrow.addEventListener("click", () => {
                 let nextBullet = arrow.classList.contains('slider__button-prev') ? currentBullet - 1 : currentBullet + 1;
+
+                if(nextBullet > 3){
+                    currentBullet = 0;
+                    nextBullet = 1;
+                }
+                if(nextBullet === 0){
+                    currentBullet = 4;
+                    nextBullet = 3;
+                }
+
                 positionSlide = (currentBullet - 1) * imgWidth;
                 wrapper.scrollLeft = arrow.classList.contains('slider__button-prev') ? positionSlide - imgWidth : positionSlide + imgWidth;
                 bullets.forEach(bullet => {
+
+                    if (currentBullet === 4){
+                        currentBullet = 1;
+                    }
+
+                    if (currentBullet === 0){
+                        currentBullet = 3;
+                    }
+
                     if (Number(bullet.dataset.bullet) === currentBullet) {
                         bullet.classList.remove('active')
                     }
@@ -26,7 +44,7 @@ if (slider) {
                     }
                 })
                 currentBullet = nextBullet;
-                disabledBullet();
+                //disabledBullet();
         });
     });
 
@@ -39,6 +57,7 @@ if (slider) {
                 };
             });
 
+
             if (currentBullet < nextBullet) {
                 let firstImgWidth = imgWidth * (nextBullet - currentBullet);
                 wrapper.scrollLeft += firstImgWidth;
@@ -49,25 +68,27 @@ if (slider) {
 
             currentBullet = nextBullet;
             bullet.classList.add('active');
-            disabledBullet();
+            //disabledBullet();
 
         });
 
 
     });
 
-    function disabledBullet(){
-        arrowPrev.classList.remove('disable')
-        arrowNext.classList.remove('disable')
-        if (currentBullet === 1) {
-            arrowPrev.classList.add('disable')
-            arrowNext.classList.remove('.disable')
-        }
-        if (currentBullet === 3) {
-            arrowPrev.classList.remove('disable')
-            arrowNext.classList.add('disable')
-        }
-    }
+    // function disabledBullet(){
+    //
+    //     arrowPrev.classList.remove('disable')
+    //     arrowNext.classList.remove('disable')
+    //     if (currentBullet === 1) {
+    //         arrowPrev.classList.add('disable')
+    //         arrowNext.classList.remove('.disable')
+    //     }
+    //     if (currentBullet === 3) {
+    //         arrowPrev.classList.remove('disable')
+    //         arrowNext.classList.add('disable')
+    //     }
+    // }
+
 
     window.addEventListener('resize', function (event) {
         wrapper.scrollLeft = 0;
